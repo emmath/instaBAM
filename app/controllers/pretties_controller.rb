5f0085b1e806ce7ef4519e30a4318d40b3e39d02
emmath <emmatrevena@gmail.com>
2013-08-26 16:58:55 -0700

    tag retrieval fixed

--- app/controllers/pretties_controller.rb
+++ app/controllers/pretties_controller.rb
@@ -1,68 +1,43 @@
 class PrettiesController < ApplicationController
-  # GET /pretties
-  # GET /pretties.json
+
   def index
    @pretties = Pretty.all
   end
 
-  # GET /pretties/1
-  # GET /pretties/1.json
   def show
-     @pretty = Pretty.get_specific
+     @pretty = Pretty.find(params[:id])
+     @images = @pretty.images
   end
 
-  # GET /pretties/new
-  # GET /pretties/new.json
   def new
     @pretty = Pretty.new
   end
 
-  # GET /pretties/1/edit
   def edit
     @pretty = Pretty.find(params[:id])
   end
 
-  # POST /pretties
-  # POST /pretties.json
   def create
     @pretty = Pretty.new(params[:pretty])
-
-    respond_to do |format|
-      if @pretty.save
-        format.html { redirect_to @pretty, notice: 'Pretty was successfully created.' }
-        format.json { render json: @pretty, status: :created, location: @pretty }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @pretty.errors, status: :unprocessable_entity }
-      end
+    if @pretty.save
+      redirect_to @pretty, notice: 'Pretty was successfully created.'
+    else
+      render action: "new"
     end
   end
 
-  # PUT /pretties/1
-  # PUT /pretties/1.json
   def update
     @pretty = Pretty.find(params[:id])
-
-    respond_to do |format|
-      if @pretty.update_attributes(params[:pretty])
-        format.html { redirect_to @pretty, notice: 'Pretty was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @pretty.errors, status: :unprocessable_entity }
-      end
+    if @pretty.update_attributes(params[:pretty])
+      redirect_to @pretty, notice: 'Pretty was successfully updated.'
+    else
+      render action: "edit"
     end
   end
 
-  # DELETE /pretties/1
-  # DELETE /pretties/1.json
   def destroy
     @pretty = Pretty.find(params[:id])
     @pretty.destroy
-
-    respond_to do |format|
-      format.html { redirect_to pretties_url }
-      format.json { head :no_content }
-    end
+      redirect_to pretties_url
   end
 end
--- app/models/pretty.rb
+++ app/models/pretty.rb
@@ -1,15 +1,14 @@
 class Pretty < ActiveRecord::Base
   attr_accessible :tag
   include HTTParty
-  base_uri 'api.instagram.com'
 
-  def self.get_specific
+  def images
     get_data['data']
   end
 
-  def self.get_data
+  def get_data
     client_id = ENV["CLIENT_ID"]
-    HTTParty.get("https://api.instagram.com/v1/tags/#{:tag}/media/recent?client_id=#{client_id}")
+    HTTParty.get("https://api.instagram.com/v1/tags/#{tag}/media/recent?client_id=#{client_id}")
   end
 end
 
